a8a0ecc4c878d305828612ced65f0e93c2ab4f81
Vladimir Tkach <teamco@gmail.com>
2020-04-11 20:17:37 +0300

    add layout

--- client/ui/app.jsx
+++ client/ui/app.jsx
@@ -1,14 +1,40 @@
 import {hot} from 'react-hot-loader';
 import React, {Component} from 'react';
-import {DatePicker} from 'antd';
+
+import {Layout, Breadcrumb} from 'antd';
+import {HeaderContent} from './components/headerContent';
+import {SiderContent} from './components/siderContent';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 class App extends Component {
   render() {
     return (
-        <div>
-          Hello World
-          <DatePicker/>
-        </div>
+        <Layout>
+          <Header>
+            <HeaderContent/>
+          </Header>
+          <Layout style={{padding: '0 24px 24px'}}>
+            <Breadcrumb style={{margin: '16px 0'}}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+                className="site-layout-background"
+                style={{
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 280
+                }}
+            >
+              Content</Content>
+            <Sider>
+              <SiderContent/>
+            </Sider>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
     );
   }
 }
---
+++ client/ui/components/headerContent.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Menu} from 'antd';
+
+export class HeaderContent extends Component {
+  render() {
+    return (
+        <div>
+          <div className="logo"/>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </div>
+    );
+  }
+}
